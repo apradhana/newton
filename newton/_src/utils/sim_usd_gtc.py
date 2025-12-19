19147a3a3807354d3fc2c7bf8391c8dd53cb1f6d
apradhana <andre.pradhana@gmail.com>
2025-12-18 22:00:01 -0800

    Change camera position, yaw, and pitch.

--- newton/_src/utils/sim_usd_gtc.py
+++ newton/_src/utils/sim_usd_gtc.py
@@ -644,6 +644,7 @@ class Simulator:
             self.viewer = newton.viewer.ViewerGL()
 
             self.viewer.set_model(self.model)
+            self.viewer.set_camera(pos=wp.vec3(15.0, 15.0, 0), pitch=-2.6, yaw=-308.0)
 
             # add a UI callback to toggle the droid is walking
             def toggle_droid_is_walking(imgui):
